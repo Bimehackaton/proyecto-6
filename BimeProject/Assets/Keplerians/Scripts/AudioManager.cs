58931175904c9bb54aae5c0a21afb6d0060db675
Keplerians <keplerianorelpek@gmail.com>
2015-10-30 23:50:46 +0100

    Jerarquia

---
+++ BimeProject/Assets/Keplerians/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager instance;
+
+	public List<CustomAudioClip> clips;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+		instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	public void PlayClip(string clipName){
+		CustomAudioClip clip = clips.Find (c => c.clipName.Equals (clipName));
+		source.PlayOneShot (clip.clip);
+	}
+}
+
+[System.Serializable]
+public class CustomAudioClip{
+	public string clipName;
+	public AudioClip clip;
+
+}
